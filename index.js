83e0e8cb927af74cb0f7e18bdc69da32538699c6
denis <denniiskim@gmail.com>
2023-11-01 04:52:49 +0300

    first set of changes

--- index.js
+++ index.js
@@ -1,81 +1,66 @@
-//functionality for back and next button in the image slider
+const dropList = document.querySelectorAll("form select"),
+fromCurrency = document.querySelector(".from select"),
+toCurrency = document.querySelector(".to select"),
+getButton = document.querySelector("form button");
 
-const slidebtn = document.getElementById("slide");
-const backbtn = document.getElementById("backBtn");
-const nextbtn = document.getElementById("nextBtn");
-
-//Array that will grab the list of images to place in the gallery
-const sliderImg = new Array(
-    "images/img1.jpg",
-    "images/img2.jpg",
-    "images/img3.jpeg",
-    "images/img4.jpg",
-    "images/img5.jpeg",
-    "images/img6.jpg"
-
-);
-
-//Array images order 0-1, therefore i = 0
-let i = 0;
-
-//function for next button. Starts from 0 the increments by 1 (i++)
-nextbtn.onclick = function() {
-   if (i < 3){
-    slide.src = sliderImg[i+1];
-    
-    i++;
-
-   } 
+for (let i = 0; i < dropList.length; i++) {
+    for(let currency_code in country_list){
+        let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "KSH" ? "selected" : "";
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+        dropList[i].insertAdjacentHTML("beforeend", optionTag);
+    }
+    dropList[i].addEventListener("change", e =>{
+        loadFlag(e.target);
+    });
 }
 
-//function for back button. Inverse of the next button i--
-backbtn.onclick = function() {
-    if (i > 0){
-     slide.src = sliderImg[i+1];
-     
-     i--;
- 
-    } 
- }
+function loadFlag(element){
+    for(let code in country_list){
+        if(code == element.value){
+            let imgTag = element.parentElement.querySelector("img");
+            imgTag.src = `https://flagcdn.com/48x36/${country_list[code].toLowerCase()}.png`;
+        }
+    }
+}
 
- /*Currency COnverter*/
- /*Declaring variables*/
+window.addEventListener("load", ()=>{
+    getExchangeRate();
+});
 
- const currency1 = document.getElementById("currency-1")
- const currency2 = document.getElementById("currency-2")
- const amount1 = document.getElementById('amount-one')
- const amount2 = document.getElementById('amount-two')
- const Convrate = document.getElementById('rate')
- const swapbutton = document.getElementById('swap')
+getButton.addEventListener("click", e =>{
+    e.preventDefault();
+    getExchangeRate();
+});
 
- /*Define an change event listeners. */
- currency1.addEventListener('change', calculate);
- currency2.addEventListener('change', calculate);
- amount1.addEventListener('input', calculate );
- amount2.addEventListener('input', calculate)
+const exchangeIcon = document.querySelector("form .icon");
+exchangeIcon.addEventListener("click", ()=>{
+    let tempCode = fromCurrency.value;
+    fromCurrency.value = toCurrency.value;
+    toCurrency.value = tempCode;
+    loadFlag(fromCurrency);
+    loadFlag(toCurrency);
+    getExchangeRate();
+})
 
- //fetch currency rates from exchangerate-api
- function calculate(){
-    const currency_1 = currency1.value;
-    const currency_2 = currency2.value; 
-   //Extract rates for the currency therefore I will add ${cuurency_1}, which will allow to fetch currency depending on whichever value I select
-    fetch(`https://v6.exchangerate-api.com/v6/218938046c0019a4b22b42f1/latest/${currency_1}`)
-    .then(response => response.json())
-    .then((data) => {
-        //  console.log(data);
-      //Conversion rate is triggered when the user clicks the swap button
-        const rate = data.conversion_rates[currency_2];
-        Convrate.innerText = `1 ${currency_1} = ${rate} ${currency_2}`;
-        amount2.value = (amount1.value * rate).toFixed(2);
-
+function getExchangeRate(){
+    const amount = document.querySelector("form input");
+    const exchangeRateTxt = document.querySelector("form .exchange-rate");
+    let amountVal = amount.value;
+    if(amountVal == "" || amountVal == "0"){
+        amount.value = "1";
+        amountVal = 1;
+    }
+    exchangeRateTxt.innerText = "Converting...";
+    /* Replace 'YOUR-API-KEY' with your own API Key by following steps writte in README.md */
+    /* let url = `https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/${fromCurrency.value}`; */
+    
+    /* Following is a example of My API Key */
+    let url = `https://v6.exchangerate-api.com/v6/218938046c0019a4b22b42f1/latest/${fromCurrency.value}`;
+    fetch(url).then(response => response.json()).then(result =>{
+        let exchangeRate = result.conversion_rates[toCurrency.value];
+        let totalExRate = (amountVal * exchangeRate).toFixed(2);
+        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+    }).catch(() =>{
+        exchangeRateTxt.innerText = "Something went wrong";
     });
- }
-
- //An event listener button that will allow the functionality of swapping currencies. Eg. GBP to USD --> USD to GBP
- swap.addEventListener('click', () => {
-    const tempVar = currency1.value;
-    currency1.value = currency2.value;
-    currency2.value = tempVar;
-    calculate();
-
- })
+}
